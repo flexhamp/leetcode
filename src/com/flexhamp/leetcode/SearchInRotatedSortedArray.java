5f62d8ae2871b9eae30df0c358452b8f1502a4a6
Kuzmenko Nikita <flexhamp@gmail.com>
2023-06-11 20:10:59 +0300

    SearchInRotatedSortedArray impl

--- src/com/flexhamp/leetcode/SearchInRotatedSortedArray.java
+++ src/com/flexhamp/leetcode/SearchInRotatedSortedArray.java
@@ -2,11 +2,12 @@ package com.flexhamp.leetcode;
 
 public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
-        int[] nums = new int[]{4,5,6,7,0,1,2};
-        int target = 13;
+        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
 
         System.out.println(search(nums, target));
     }
+
     /*
     There is an integer array nums sorted in ascending order (with distinct values).
 
@@ -41,8 +42,31 @@ public class SearchInRotatedSortedArray {
     -104 <= target <= 104
     */
     public static int search(int[] nums, int target) {
-        int res = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        int middle;
 
-        return res;
+        while (left <= right) {
+            middle = (left + right) / 2;
+            if (target == nums[middle]) {
+                return middle;
+            }
+
+            if (nums[left] <= nums[middle]) {
+                if (target > nums[middle] || target < nums[left]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            } else {
+                if (target < nums[middle] || target > nums[right]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            }
+        }
+
+        return -1;
     }
 }
