ab54f013d564d4a6bf827efec57df5619846a7a1
Kuzmenko Nikita <flexhamp@gmail.com>
2023-06-12 09:53:13 +0300

    TwoSum impl

--- src/com/flexhamp/leetcode/TwoSum.java
+++ src/com/flexhamp/leetcode/TwoSum.java
@@ -1,6 +1,8 @@
 package com.flexhamp.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -30,7 +32,7 @@ public class TwoSum {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
 
-        System.out.println(Arrays.toString(twoSum(nums, target)));
+        System.out.println(Arrays.toString(twoSum3(nums, target)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -51,16 +53,30 @@ public class TwoSum {
             }
         }
         return new int[]{};
-// best ?
-//        int j = 1;
-//        for (int i = 0; i < nums.length && (i + j) < nums.length; i++) {
-//            if (nums[i] + nums[i + j] == target) {
-//                return new int[]{i, i + j};
-//            } else if (i == nums.length - 1 - j) {
-//                j++;
-//                i = -1;
-//            }
-//        }
-//        return null;
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        int j = 1;
+        for (int i = 0; i < nums.length && (i + j) < nums.length; i++) {
+            if (nums[i] + nums[i + j] == target) {
+                return new int[]{i, i + j};
+            } else if (i == nums.length - 1 - j) {
+                j++;
+                i = -1;
+            }
+        }
+        return new int[]{};
+    }
+
+    public static int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
     }
 }
