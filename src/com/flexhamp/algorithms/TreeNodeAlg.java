8885188be7925df7c3096b20855b64e5dea7b758
Kuzmenko Nikita <flexhamp@gmail.com>
2023-06-12 17:40:46 +0300

    maxPathSum2 impl

--- src/com/flexhamp/algorithms/TreeNodeAlg.java
+++ src/com/flexhamp/algorithms/TreeNodeAlg.java
@@ -3,11 +3,27 @@ package com.flexhamp.algorithms;
 import com.flexhamp.util.TreeNode;
 
 public class TreeNodeAlg {
-    public static void main(String[] args) {
-        final TreeNode treeNode = new TreeNode(1, new TreeNode(4, new TreeNode(2), new TreeNode(3, new TreeNode(2))),
-                new TreeNode(7, new TreeNode(5), new TreeNode(4)));
+    static int answer = 0;
 
-        System.out.println(maxPathSum(treeNode));
+    public static void main(String[] args) {
+//        final TreeNode treeNode = new TreeNode(1, new TreeNode(4, new TreeNode(2), new TreeNode(3, new TreeNode(2))),
+//                new TreeNode(7, new TreeNode(5), new TreeNode(4)));
+//
+//        System.out.println(maxPathSum(treeNode));
+
+
+        final TreeNode treeNode2 = new TreeNode(
+                -10,
+                new TreeNode(9),
+                new TreeNode(20,
+                        new TreeNode(-3,
+                                new TreeNode(5,
+                                        new TreeNode(-2)),
+                                new TreeNode(-4)),
+                        new TreeNode(8, new TreeNode(-2)))
+        );
+
+        System.out.println(maxPathSum2(treeNode2));
     }
 
     /*
@@ -25,4 +41,26 @@ public class TreeNodeAlg {
         return Math.max(maxLeftPath, maxRightPath) + root.val;
     }
 
+    /*
+    O(n) - сложность по времени
+    O(n) - сложность по памяти
+
+    */
+    //    https://www.youtube.com/watch?v=R4UHOLZ-bEk
+    public static int maxPathSum2(TreeNode root) {
+        answer = 0;
+        helper(root);
+        return answer;
+    }
+
+    private static int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int maxLeftPath = Math.max(helper(root.left), 0);
+        int maxRightPath = Math.max(helper(root.right), 0);
+        answer = Math.max(answer, maxLeftPath + maxRightPath + root.val);
+        return Math.max(maxLeftPath, maxRightPath) + root.val;
+    }
 }
